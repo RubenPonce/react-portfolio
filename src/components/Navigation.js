0ed350bc4ab29627cbc93d13d10de19427513bce
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-05-12 14:06:28 -0700

    added scrolling animation to the nav bar, and i promise the contact form functionality is next

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import {Route} from 'react-router-dom';
-export default function Navigation() {
+export default class Navigation extends React.Component {
     
-  return (
-    <div className="header">
+    handleScroll = (event) =>{
+        if(window.scrollY > 10){
+            if(document.querySelector('.header').classList.contains('remove-scroll')){
+                document.querySelector('.header').classList.remove('remove-scroll')
+            }
+            
+            document.querySelector('.header').classList.add('scroll-effect');
+        } else{
+            document.querySelector('.header').classList.remove('scroll-effect');
+            document.querySelector('.header').classList.add('remove-scroll')
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+      }
+    
+      componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+      }
+
+  render(){return (
+    <div className="header " onScroll={this.handleScroll}>
     <div  className="nav">
           <Route path="/project/:id" render={()=><Link to="/"  >Projects </Link>} />
           <a href="https://sourcerer.io/rubenponce" target="_blank" rel="noopener noreferrer">Technologies Used</a>
@@ -18,5 +38,5 @@ export default function Navigation() {
    
     </div>
     </div>
-  )
+  )}
 }
